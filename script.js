125390de32c1bb39e2bf7a539b7fd35eaad175c8
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-25 17:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let num1 = document.getElementById('n1');
 let num2 = document.getElementById('n2');
 let btn = document.getElementById('sum_btn');
-let sum = document.getElementById('sum');
+let sum = document.getElementById('Sum');
 
 btn.addEventListener('click' ,function(){
 	let ans = parseInt(num1.value) + parseInt(num2.value);
